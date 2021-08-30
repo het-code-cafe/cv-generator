f0c9842def99ffb4bc55bd96a1e6fcdca5902a11
Code Café <77829869+het-code-cafe@users.noreply.github.com>
2021-08-30 12:32:38 +0200

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -2,4 +2,4 @@
 
 pandoc -s --template="./templates/default.html" -f markdown-smart -c style/default.css "resume.md" -o "resume.html"
 wkhtmltopdf --enable-local-file-access "resume.html" "resume.pdf"
-rm ./resume.html
+rm resume.html
